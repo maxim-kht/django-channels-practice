0826b82ae547467f36b391e7d28a764971d92644
Maxim Kukhtenkov <maxim.kht@gmail.com>
2016-09-26 22:22:29 -0400

    Rename Project

--- channels_chat/urls.py
+++ channels_chat/urls.py
@@ -1,4 +1,4 @@
-"""channels_tutorial URL Configuration
+"""channels_chat URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
